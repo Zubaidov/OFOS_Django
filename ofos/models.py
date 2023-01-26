14d8b150b1b0ff08ecb3b4f60b769054416b3b1e
Zubaidov <shohruz.zubaidov@gmail.com>
2023-01-27 02:49:38 +0400

    configures database

--- ofos/models.py
+++ ofos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class FoodModel(models.Model):
+    #offer : bool
+    #id : int
+    #name : str
+    #img : str
+    #desc : str
+    #price : int
+    img = models.ImageField(upload_to='images')
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=500)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+
 # Create your models here.
