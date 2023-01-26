6bf782984f8d6b76456d17ea9ea96a04379fc999
Zubaidov <shohruz.zubaidov@gmail.com>
2023-01-26 22:38:34 +0400

    Hello

--- ofos/urls.py
+++ ofos/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -8,3 +10,7 @@ urlpatterns = [
     path('faq/', views.faq, name="FAQ"),
     path('add/', views.add, name='add')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
